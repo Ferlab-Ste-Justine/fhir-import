5e0df0ad7022fa1f6b20260554ed37d676a56764
Eric Vallee <eric_vallee@webificservices.com>
2020-04-17 12:40:06 -0400

    Removal of extra rows for extra patients. Image caching optimization to reduce build time on code changes.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,9 @@
 FROM python:3.8
 
-COPY . /opt
-
 WORKDIR /opt
 
+COPY requirements.txt /opt
+
 RUN pip install -r requirements.txt
+
+COPY . /opt
--- patient.py
+++ patient.py
@@ -14,7 +14,7 @@ SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly']
 
 # The ID and range of a sample spreadsheet.
 SAMPLE_SPREADSHEET_ID = '1jAP_jpsGA6GS_qFcYwt6TdppW4QU4qCxKocOiZ8Up8g'
-SAMPLE_RANGE_NAME = 'patient!B1:V151'
+SAMPLE_RANGE_NAME = 'patient!B1:V53'
 
 
 def main(args):
